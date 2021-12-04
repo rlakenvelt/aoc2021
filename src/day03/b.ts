1a7bdac3dc1379f3740708c4a2319debf646aefd
Roel Lakenvelt <r.lakenvelt@flusso.nl>
2021-12-04 10:09:55 +0100

    Day 3 titles

--- src/day03/b.ts
+++ src/day03/b.ts
@@ -1,7 +1,7 @@
 import InputHelper from '../utils/input';
 import Logger from '../utils/logger';
 
-const puzzle = 'Day 03B'
+const puzzle = 'Day 03B: Binary Diagnostic'
 const testmode = false;
 const input = new InputHelper(testmode);
 const logger = new Logger(puzzle, testmode);
