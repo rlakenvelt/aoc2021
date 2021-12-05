9cdeecc0cf1e91f6273983d9087de7c46fa6a970
Roel Lakenvelt <r.lakenvelt@flusso.nl>
2021-12-05 11:01:31 +0100

    Day 5

---
+++ src/day05/b.ts
@@ -0,0 +1,33 @@
+import InputHelper from '../utils/input';
+import Logger from '../utils/logger';
+import {Grid, Line, Point} from './grid';
+
+const puzzle = 'Day 05B: Hydrothermal Venture'
+const input = new InputHelper();
+const logger = new Logger(puzzle);
+
+logger.start();
+
+let lines: Line []   = input.getInput()
+                            .reduce((list: Line[], line) => {
+                                const parts = line.split(' -> ');
+                                const start = new Point(parseInt(parts[0].split(',')[0]), parseInt(parts[0].split(',')[1]));
+                                const end = new Point(parseInt(parts[1].split(',')[0]), parseInt(parts[1].split(',')[1]));
+                                const newline = new Line(start, end);
+                                list.push(newline);
+                                return list;
+                            }, []);
+const height = lines.reduce((h, p) => {
+                        h = Math.max(h, p.from.y, p.to.y);
+                        return h;
+                    }, 1);
+const width = lines.reduce((w, p) => {
+                        w = Math.max(w, p.from.x, p.to.x);
+                        return w;
+                    }, 0);
+
+const grid = new Grid(height+1, width+1);
+grid.markLines(lines);
+// grid.display();
+logger.end(grid.countOverlapping());
+
