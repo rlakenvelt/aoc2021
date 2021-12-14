70583194343ad02f1340069ef711996a42a4833e
Roel Lakenvelt <r.lakenvelt@flusso.nl>
2021-12-14 17:24:49 +0100

    Remove debug logging

--- src/day14/a.ts
+++ src/day14/a.ts
@@ -17,8 +17,6 @@ const rules: Rule[] = inputValues[1].split('\n').map(s=>{
     const parts = s.split(' -> ');
     return {pair: parts[0], insert: parts[1]}
 });
-console.log(template);
-console.log(rules);
 
 let answer = 0;
 const steps = 10;
@@ -26,12 +24,10 @@ for (let step=0; step<steps; step++) {
     for (let i=template.length-2; i>=0; i--) {
         const search = template[i]+template[i+1];
         const rule = rules.find(r=>r.pair===search);
-        // console.log(i, search, rule);
         if (rule) {
             template.splice(i+1,0,rule.insert); 
         }
     }
-    // console.log(template.join(''));
 }
 const counts: any = {};
 template.forEach(c=> {
@@ -40,7 +36,6 @@ template.forEach(c=> {
 })
 
 const temp:number[] = Object.keys(counts).map(c=>counts[c]).sort((a,b)=>a-b);
-console.log(temp);
 answer=temp[temp.length-1]-temp[0];
 
 
