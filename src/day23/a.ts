b7d42668bbc905a334af94fab6f8e2f2a5bf7a8e
Roel Lakenvelt <r.lakenvelt@flusso.nl>
2022-01-02 14:41:40 +0100

    Day 23 part 1

--- src/day23/a.ts
+++ src/day23/a.ts
@@ -4,19 +4,24 @@ import Logger from '../utils/logger';
 const puzzle = 'Day 23A'
 const input = new InputHelper();
 const logger = new Logger(puzzle);
-const rooms: string[] = ['A', 'B', 'C', 'D'];
+const rooms: string[] = ['','','','A','','B','','C','','D','',''];
 let maxY = 1;
+
+type Path = {
+    endpoint: Point, 
+    path: string[], 
+    length: number
+}
 
 class Amphipod {
     id: number;
-    type: string;
-    // location: Point
+    room: string;
     energyConsumption: number;
-    constructor(id: number, type: string) {
-        this.type = type;
+    constructor(id: number, room: string) {
+        this.room = room;
         this.id = id;
         this.energyConsumption = 1;
-        switch (type) {
+        switch (room) {
             case 'B': 
                 this.energyConsumption = 10;
                 break;
@@ -27,41 +32,41 @@ class Amphipod {
                 this.energyConsumption = 1000;
                 break;
         }
-        // this.location=new Point(x,y);
     }
     locationOnMap(map: any): Point {
-        let hash = '';
+        let hash: string = '';
         Object.keys(map).forEach(key=> {
-            if (map[key]===this.id) {
+            if (map[key].id===this.id) {
                 hash=key;
             }
         })
-        const coord = hash.split('_').map(i=>parseInt(i))
-        return new Point(coord[0], coord[1]);
+        return points.find(p=>p.hash===hash) || new Point(0,0);
     }
-    endpointCostsUsingMap(point: Point, map: any): number {
-        if (['3_1','5_1','7_1','9_1'].includes(hash(point.x, point.y))) return -1; // On room exit
-        if (point.y>1) {
-            if (this.type!==point.room) return -2; // To wrong room
-            for (let y=point.y+1; y<=maxY; y++) {
-                if (map[hash(point.x, y)]<0) return -3; // Room not completely filled
+    endpointCostsUsingMap(location: Point, path: Path, map: any): number {
+
+        if (path.endpoint.y>1) {
+            if (this.room!==path.endpoint.room) return -3; // To wrong room
+            for (let y=path.endpoint.y+1; y<=maxY; y++) {
+                const id = map[Point.hash(path.endpoint.x, y)].id;
+                if (id<0) return -5; // Room not completely filled
+                if (amphipods[id].room!==this.room) return -6; // Wrong amphipod in room
             }
         }
-        const location: Point = this.locationOnMap(map);
-        if (location.y>1&&this.type===location.room) {
+        if (this.room===location.room) {
             let allInPlace = true;
             for (let y=location.y+1; y<=maxY; y++) {
-                const id = map[hash(location.x, y)];
-                if (map[id]<0||amiphods[id].type!==this.type) {
+                const id = map[Point.hash(location.x, y)].id;
+                if (id<0||amphipods[id].room!==this.room) {
                     allInPlace=false;
                 }
             }
-            if (allInPlace) return -4; // Already in place
+            if (allInPlace) return -7; // Already in place
 
         }
-        if (location.y===1&&point.y===1) return -5; // from hall to hall
-        const path = location.path(point);
-        if (!path.every(p=> map[p]<0)) return -6; // Amphipod on path
+        if (!path.path.every(p=> map[p].id<0)) {
+            return -8; // Amphipod on path
+        }
+
         return path.length*this.energyConsumption;
     }
 }
@@ -69,32 +74,45 @@ class Amphipod {
 class Point {
     x: number;
     y: number;
+    paths: Path[] = [];
+    hash: string;
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
+        this.hash = Point.hash(this.x, this.y);
     }
     get isInHallway() {
         return this.y===1;
     }
     get room(): string {
         if (this.y===1) return '';
-        return rooms[(this.x-1)/2-1];
+        return rooms[this.x];
     }
-    distance(point: Point): number {
-        return this.path(point).length;
+
+    static hash(x:number, y:number): string {
+        return `${x}_${y}`;
     }
-    path(point: Point): string[] {
+    calculatePaths() {
+        points.forEach(point=> {
+            if (['3_1','5_1','7_1','9_1'].includes(point.hash)) return; // Roon entry
+            if (this.y===1&&point.y===1) return; // From hall to hall
+            if (this.x===point.x) return; // To same room
+            const path = this.path(point);
+            this.paths.push({endpoint: point, path: path, length: path.length})
+        })
+    }
+    private path(point: Point): string[] {
         let points: string[] = [];
         for (let y=1; y<=this.y; y++) {
-            points.push(hash(this.x,y));
+            points.push(Point.hash(this.x,y));
         }
         for (let y=1; y<=point.y; y++) {
-            points.push(hash(point.x,y));
+            points.push(Point.hash(point.x,y));
         }
         for (let x = Math.min(this.x, point.x); x<=Math.max(this.x, point.x); x++) {
-            points.push(hash(x,1));
+            points.push(Point.hash(x,1));
         }
-        return [...new Set(points.filter(p=>p!==hash(this.x, this.y))) ];
+        return [...new Set(points.filter(p=>p!==this.hash)) ];
     }
 }
 
@@ -102,90 +120,102 @@ logger.start();
 
 const inputValues: string[][] = input.getInput().map(x=>x.split(''));
 const burrowMap: any = {};
-const amiphods: Amphipod[] = [];
+const amphipods: Amphipod[] = [];
 const points: Point[] = [];
-let id = 0;
-for (let x = 0; x<inputValues[0].length; x++) {
-    for (let y = 0; y<inputValues.length; y++) {
-        switch (inputValues[y][x]) {
-            case '.':
-                burrowMap[hash(x,y)] = -1;
-                points.push(new Point(x,y));
-                break;
-            case 'A':
-            case 'B':
-            case 'C':
-            case 'D':
-                const newAmphipod = new Amphipod(id, inputValues[y][x]);
-                burrowMap[hash(x,y)] = id;
-                amiphods.push(newAmphipod);
-                points.push(new Point(x,y));
-                id++;
-                maxY = Math.max(maxY, y);
-                break;
-        }
-    }
 
-}
+init();
 
-let answer = move(burrowMap, 0, '');
+let answer = move(burrowMap, 0);
 
 logger.end(answer);
 
-function move(map: any, level: number, path: string): number {
-    console.log('LEVEL', level)
+function move(map: any, level: number): number {
     let possibleMoves: any[] = [];
-    // if (level>1) return 0;
     let energy = Infinity;
-    amiphods.forEach(amphipod=> {
-        points.forEach(point=> {
-            const costs = amphipod.endpointCostsUsingMap(point, map);
+    let endpoints = false;
+    amphipods.forEach(amphipod=> {
+        const location: Point = amphipod.locationOnMap(map);
+
+        location.paths.forEach(path=> {
+            const costs = amphipod.endpointCostsUsingMap(location, path, map);
             if (costs > 0) {
-                possibleMoves.push({id: amphipod.id, point: point, energy: costs})
+                const isEndpoint = isAtEndpoint(path.endpoint, map);
+                if (isEndpoint) endpoints = true;
+                possibleMoves.push({id: amphipod.id, point: path.endpoint, energy: costs, end: isEndpoint})
             }
         })
     })
-    if (possibleMoves.length===0) return 0;
-    // if (level>100) {
-    //     console.log(map)
-    //     console.log(possibleMoves);
-    //     return 0;
-    // }
-    // possibleMoves=[possibleMoves[0]];
-    // console.log(possibleMoves);
-
-    for (let m of possibleMoves) {
-        const newMap = {...map};
-        Object.keys(newMap).forEach(key=> {
-            if (newMap[key]===m.id) newMap[key]=-1;
-        })
-        newMap[hash(m.point.x, m.point.y)] = m.id;
-        const e = move(newMap, level+1, '');
-        energy = Math.min(m.energy, m.energy + e);
+    if (endpoints) {
+        possibleMoves = possibleMoves.filter(move=>move.end=== true)
     }
 
+    if (possibleMoves.length===0) {
+        let done = true
+        for (let amphipod of amphipods) {
+            const location: Point = amphipod.locationOnMap(map);
+            if (location.room!==amphipod.room) done = false
+        }
+      
+        return (done?0:Infinity);
+    }
+
+    for (let m of possibleMoves) {
+
+        const newMap = {...map};
+        Object.keys(newMap).forEach(key=> {
+            if (newMap[key].id===m.id) {
+                newMap[key]={id:-1,room:''};
+            }              
+        })
+
+        newMap[m.point.hash] = {id:m.id,room:amphipods[m.id].room};
+
+        const e = move(newMap, level+1);
+        if (e!==Infinity)
+            energy = Math.min(energy, m.energy + e);
+
+    }
+    if (level<=1) {
+        console.log(level, energy);
+
+    }
     return energy;
 }
 
-
-
-function hash(x:number,y:number): string {
-    return `${x}_${y}`;
+function isAtEndpoint(point: Point, map: any) {
+    if (point.y===1) return false;
+    for (let y=point.y+1; y<=maxY; y++) {
+        if (map[point.hash].id<0) return false;
+    }  
+    return true;  
 }
 
-// console.log(amiphods)
-// const newmap = {...burrowMap};
-// newmap['1_1']=10;
-// console.log(burrowMap)
-// console.log(newmap)
+function init () {
 
-// console.log(points)
-// console.log(burrowMap)
-// console.log(amiphods)
-// const p1 = new Point(6,1);
-// const p2 = new Point(7,3);
-// console.log(p1.distance(p2));
-// console.log(p1.path(p2));
-// console.log(p1.room);
-// console.log(p1.isInHallway);
-// console.log(amiphods[0].endpointCosts(p1));
+    let id = 0;
+    for (let x = 0; x<inputValues[0].length; x++) {
+        for (let y = 0; y<inputValues.length; y++) {
+            const point = new Point(x,y);
+            switch (inputValues[y][x]) {
+                case '.':
+                    burrowMap[point.hash] = {id: -1, room: ''};
+                    points.push(point);
+                    break;
+                case 'A':
+                case 'B':
+                case 'C':
+                case 'D':
+                    const newAmphipod = new Amphipod(id, inputValues[y][x]);
+                    burrowMap[point.hash] = {id: id, room: newAmphipod.room};
+                    amphipods.push(newAmphipod);
+                    points.push(point);
+                    id++;
+                    maxY = Math.max(maxY, y);
+                    break;
+            }
+        }
+    }
+    points.forEach(point=> {
+        point.calculatePaths();
+    })    
+}
--- src/day23/b.ts
+++ src/day23/b.ts
@@ -14,3 +14,27 @@ let answer = 0;
 
 logger.end(answer);
 
+
+// console.log(amphipods)
+// const newmap = {...burrowMap};
+// newmap['1_1']={id: 10, room: 'BLA'};
+// console.log(burrowMap)
+// console.log(newmap)
+
+// console.log(points)
+// console.log(burrowMap)
+// console.log(amphipods)
+// const p1 = new Point(3,3);
+// const p2 = new Point(1,1);
+// console.log(p1.distance(p2));
+// console.log(p1.path(p2));
+// console.log(p1.room);
+// console.log(p1.isInHallway);
+// console.log(amphipods[0].endpointCosts(p1));
+// const a=amphipods[2];
+// console.log(a)
+// points.forEach(point=> {
+//     const costs = a.endpointCostsUsingMap(point, burrowMap);
+//     console.log(point, costs);
+
+// })
